cea912944f5972ac4dc0215d5cdc57232cce7869
GPointExtreme <dominik.geiger@hotmail.com>
2015-10-11 15:05:00 +0200

    VariableDemo geschafft!

---
+++ src/VariableDemo.java
@@ -0,0 +1,15 @@
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String Vorname="Dominik";
+		String Nachname="Geiger";
+
+		System.out.println("Der Name lautet: " + (Vorname) + " "+ (Nachname));
+		
+		
+	}
+
+}
