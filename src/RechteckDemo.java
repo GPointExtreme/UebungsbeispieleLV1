e7f0e2f97eaa20f5cfd486870b6ada93a6caa71c
GPointExtreme <dominik.geiger@hotmail.com>
2015-10-12 17:31:11 +0200

    Übungbeispiel 2 geschafft!

---
+++ src/RechteckDemo.java
@@ -0,0 +1,18 @@
+
+public class RechteckDemo {
+
+	public static void main(String[] args) {
+		// Übungsaufgabe 2
+		
+		int breite=4;
+		int laenge=5;
+		
+		int umfang=(breite + laenge) * 2;
+		int flaeche=(breite * laenge);
+				
+			System.out.println("Umfang: " + umfang);	
+			System.out.println("Fläche: " + flaeche);
+
+	}
+
+}
